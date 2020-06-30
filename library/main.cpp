a6f7cbdd45ac9ae45629b4233050ddfb8ecb0db5
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-06-30 20:35:04 +0545

    Update main.cpp

--- library/main.cpp
+++ library/main.cpp
@@ -8,8 +8,8 @@
 #define gc getchar_unlocked
 #define ll long long
 #define ld long double
-#define pii std::pair<int,int>
-#define pl std::pair<long,long>
+#define pii std::pair<int, int>
+#define pl std::pair<long, long>
 #define vi std::vector<int>
 #define vl std::vector<ll>
 #define vpii std::vector<pii>
@@ -27,54 +27,55 @@
 #define S second
 
 #define PI 3.14159265358979323846264
-#define MOD 1e9+7
+#define MOD 1e9 + 7
 #define INF 1e18
 
 #define fo(i, n) for (ll i = 0; i < n; ++i)
-#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i: --i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
 #define deb(x) cout << #x << "=" << x << endl
 #define sortall(x) sort(all(x))
-#define tr(it, a) for(auto it = a.begin(); it != a.end(); ++it)
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); ++it)
 #define ps(x, y) std::fixed << setprecision(y) << x
 #define setbits(x) __builtin_popcountll(x)
 #define zrobits(x) __builtin_ctzll(x)
-#define mk(arr, n, type) type *arr = new type[n]
+#define mk(arr, n, type) type* arr = new type[n]
 
 std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
-void setup(){     
+inline void setup() {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
     std::cout.tie(0);
 
-    #ifdef LOCAL_PROJECT                    //run with -DLOCAL_PROJECT drung compilation
-        freopen("input.txt", "r", stdin);
-    #else
-        #ifndef ONLINE_JUDGE               //runs automatically for supported online judges
-            freopen("input.txt", "r", stdin);
-            freopen("output.txt", "w", stdout);
-        #endif
-    #endif 
+#ifdef LOCAL_PROJECT  //run with -DLOCAL_PROJECT drung compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  //runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+#endif
 }
 
-void solve();
+inline void solve();
 
-int main(){
+int main() {
     setup();
     ll t = 1;
 
-    std::cin>>t;    //Comment this if testCases = 1
+    ll* n = &t;
 
-    while(t--){
+    std::cin >> t;  //Comment this if testCases = 1
+
+    while (t--) {
         solve();
-        std::cout<<'\n';
     }
     return 0;
 }
 
 using namespace std;
 
-void solve(){
+inline void solve() {
 }
