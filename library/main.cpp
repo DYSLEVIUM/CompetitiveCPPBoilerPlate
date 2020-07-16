9ba86358feb5c272bb113b054191085e93d2577f
Pushpakant Behera <38003829+Pushpakant@users.noreply.github.com>
2020-07-16 17:58:57 +0545

    Update main.cpp

--- library/main.cpp
+++ library/main.cpp
@@ -89,16 +89,16 @@ int main() {
 
     std::cin >> t;
 
-    clock_t startT = clock();
+//     clock_t startT = clock();
 
     while (t--) {
         solve();
     }
     
-    clock_t endT = clock();
+//     clock_t endT = clock();
 
-    std::cout << '\n'
-              << ((double)endT - startT) / CLOCKS_PER_SEC;
+//     std::cout << '\n'
+//               << ((double)endT - startT) / CLOCKS_PER_SEC;
     return 0;
 }
 
