11cdc379b31e8739d04de9b7971cbdde9656f56b
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-03-17 21:37:04 +0545

    Update and rename main.cpp to main.cc

---
+++ template/main.cc
@@ -0,0 +1,45 @@
+#define _USE_MATH_DEFINES
+#pragma GCC optimize("Ofast,fast-math")
+
+#include <bits/stdc++.h>
+
+#define deb(x) cout << '\n' \
+                    << #x << " = " << x << '\n'
+
+inline void setup() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+    freopen("input.txt", "r", stdin);
+#else
+#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+    freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+#endif
+#endif
+}
+
+inline void solve();
+
+int main(int argc, char* argv[]) {
+    setup();
+
+    long long t = 1;
+    std::cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
+
+//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
+//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
+//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+
+using namespace std;
+
+inline void solve() {
+}
