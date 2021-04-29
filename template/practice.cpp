ee6e8d267238ae927f6c8c7d6bcf1dc58b02f3d3
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-04-29 16:05:20 +0545

    Update practice.cpp

--- template/practice.cpp
+++ template/practice.cpp
@@ -20,7 +20,7 @@ typedef std::priority_queue<int, vi, std::greater<int>> pqi;
 #define eb emplace_back
 #define F first
 #define S second
-#define MOD (long long)1e9 + 7
+#define MOD (ll)(1e9 + 7)
 #define PI 3.14159265358979323846
 #define INF __builtin_inff()
 
@@ -47,10 +47,6 @@ inline void setup() {
 #ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
     freopen("input.txt", "r", stdin);
 #else
-#ifndef ONLINE_JUDGE  // runs automatically for supported online judges
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-#endif
 #endif
 }
 
@@ -60,16 +56,20 @@ int main(int argc, char* argv[]) {
     setup();
 
     ll t = 1;
-    // std::cin >> t;
+    std::cin >> t;
 
-    while (t--)
+    ll tempT = t;
+    while (t--) {
+        std::cout << "Case #" << tempT - t << ": ";
         solve();
+        std::cout << '\n';
+    }
 
     return 0;
 }
 
 using namespace std;
-//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -o a.exe -Ofast -Wno-unused-result && ./a.exe
+//  Compile and run: g++ -g -Wshadow -Wall -DLOCAL_PROJECT practice.cpp -o a.exe -Ofast -Wno-unused-result && ./a.exe
 
 inline void solve() {
 }
