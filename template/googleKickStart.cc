b88f197121297457da597f4f6b32e97db0377cf0
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-20 20:17:20 +0530

    Update googleKickStart.cc

--- template/googleKickStart.cc
+++ template/googleKickStart.cc
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
 }
 
 using namespace std;
-//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall practice.cpp -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
+//  Compile and run: g++ -std=c++17 -g -Wshadow -Wall main.cc -D DYSLEVIUM -o a -Ofast -Wno-unused-result && ./a
 
 inline void solve() {
 }
