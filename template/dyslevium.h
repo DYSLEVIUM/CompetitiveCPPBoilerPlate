0e3ab8f04a808718c0580da08cd6c504be77a666
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-21 00:47:13 +0530

    Add files via upload

---
+++ template/dyslevium.h
@@ -0,0 +1,28 @@
+#ifndef DYSLEVIUM_H
+#define DYSLEVIUM_H
+
+#include <algorithm>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#endif
