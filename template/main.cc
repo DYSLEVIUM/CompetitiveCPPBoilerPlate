10a8a7578e91af54dc02069484b41bcd90053058
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-06-20 20:04:21 +0530

    Update main.cc

--- template/main.cc
+++ template/main.cc
@@ -3,17 +3,24 @@
 
 #include <bits/stdc++.h>
 
-#define deb(x) cout << '\n' \
-                    << #x << " = " << x << '\n'
+//  debuging
+#ifdef DYSLEVIUM
+#define deb(x) std::cerr << '\n' \
+                         << #x << " = " << x << '\n'
+#else
+#define deb(x)
+#endif
 
+//  initial setup
 inline void setup() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
 
-#ifdef LOCAL_PROJECT  // run with -DLOCAL_PROJECT during compilation
+#ifdef DYSLEVIUM
   freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+  freopen("output.txt", "w", stdout);
+  freopen("error.txt", "w", stderr);
 #endif
 }
 
@@ -30,10 +37,7 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-//Compile: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result
-//Build: g++ -g -Wshadow -Wall main.cc -o a.exe -D_GLIBCXX_DEBUG
-//Compile and run: g++ -g -Wshadow -Wall main.cc -o a.exe -Ofast -Wno-unused-result && ./a.exe
+//  Compile and run: g++ -g -Wshadow -Wall practice.cpp -DDYSLEVIUM-o a -Ofast -Wno-unused-result && ./a
 
 using namespace std;
 
