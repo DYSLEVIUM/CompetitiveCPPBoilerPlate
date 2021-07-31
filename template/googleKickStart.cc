ea9524d348eddb23ddedd401d79dfdd03801619d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-07-31 15:06:56 +0545

    Update googleKickStart.cc

--- template/googleKickStart.cc
+++ template/googleKickStart.cc
@@ -43,6 +43,7 @@ inline void setup() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
+  std::cerr.tie(nullptr);
 
 #ifdef DYSLEVIUM
   freopen("input.in", "r", stdin);
