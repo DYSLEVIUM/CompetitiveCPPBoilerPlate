d369c7883ffc94af5f5516f8f5d14f665671d488
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-08-10 18:53:41 +0545

    Update main.cc

--- template/main.cc
+++ template/main.cc
@@ -19,8 +19,13 @@ typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
 
 #define pb push_back
 #define eb emplace_back
+#define F first
+#define S second
 #define MOD (ll)(1e9 + 7)
 
+std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+//  macro functions
 #define fo(i, n) for (ll i = 0; i < (ll)n; ++i)
 #define Fo(i, k, n) for (ll i = k; k < (ll)n ? i < (ll)n : i > (ll)n; k < (ll)n ? ++i : --i)
 #define all(x) x.begin(), x.end()
@@ -28,8 +33,30 @@ typedef std::priority_queue<ll, vl, std::greater<ll>> pqi;
 #define ps(x, y) std::fixed << std::setprecision(y) << x
 #define setbits(x) __builtin_popcountll(x)
 #define zerobits(x) __builtin_ctzll(x)
+#define modAdd(a, b) ((((a % MOD) + (b % MOD)) % MOD) + MOD) % MOD
+#define modSub(a, b) ((((a % MOD) - (b % MOD)) % MOD) + MOD) % MOD
+#define modMul(a, b) ((((a % MOD) * (b % MOD)) % MOD) + MOD) % MOD
 
-std::mt19937_64 rng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+//  template functions
+template <typename T>
+inline T gcd(const T& a, const T& b) {
+  if (b) return gcd(b, a % b);
+  return a;
+}
+template <typename T>
+inline T binPowIter(T x, T n) {
+  T res = 1;
+  while (n) {
+    if (n & 1) res = modMul(res, x);
+    x = modMul(x, x);
+    n >>= 1;
+  }
+  return res % MOD;
+}
+template <typename T>
+inline T modInverse(const T& a) { return binPowIter(a, MOD - 2); }
+template <typename T>
+inline T modDiv(const T& a, const T& b) { return (modMul(a, modInverse(b)) + MOD) % MOD; }
 
 //  debuging
 #ifdef DYSLEVIUM
@@ -57,10 +84,20 @@ inline void solve();
 int main(int argc, char* argv[]) {
   setup();
 
+  auto startTime = std::chrono::high_resolution_clock::now();
+
   ll t = 1;
-  std::cin >> t;
+  // std::cin >> t;
 
   while (t--) solve();
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+#ifdef DYSLEVIUM
+  std::cerr << "Time: " << duration.count();
+#endif
 
   return 0;
 }
