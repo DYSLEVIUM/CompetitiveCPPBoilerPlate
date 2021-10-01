6c2a7a68950a601b6e2f6f1152ef45655deb5f6d
Pushpakant Behera <pushpakantbehera@hotmail.com>
2021-10-02 01:08:55 +0545

    Update main.cc

--- template/main.cc
+++ template/main.cc
@@ -80,8 +80,6 @@ inline T modDiv(const T& a, const T& b) {
 inline void setup() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
-  std::cerr.tie(nullptr);
 
 #ifdef DYSLEVIUM
   freopen("input.in", "r", stdin);
