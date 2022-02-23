8f3ee9b3b68ada1be5a9fe01e6999777f8a389a0
Pushpakant Behera <pushpakantbehera@hotmail.com>
2022-02-23 17:29:50 +0530

    Add files via upload

---
+++ template/file_watcher.cc
@@ -0,0 +1,14 @@
+#include "dyslevium.h"
+
+int main(int argc, char* argv[]) {
+  std::vector<std::string> files{"./main.cc", "./input.in"};
+  std::chrono::duration<int, std::milli> delay = std::chrono::milliseconds(500);
+
+  File_Watcher main_file(files, delay);
+  auto commands = [] {
+    system("clear");
+    system("time make run");
+  };
+
+  main_file.watch(commands);
+}
