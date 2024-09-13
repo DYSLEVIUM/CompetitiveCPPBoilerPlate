1a05d9bab54cfb12e0ee9a6818cc1cb6587fc5e3
Pushpakant Behera <pushpakantbehera@hotmail.com>
2024-09-13 17:39:34 +0530

    Update main.cc

--- template/main.cc
+++ template/main.cc
@@ -1,7 +1,7 @@
 /*
-  DYSLEVIUM's template
-  Date: 02/February/2023
-  Time: 17:13:16
+    DYSLEVIUM's template
+    Date: 13/September/2024
+    Time: 17:39:06
 */
 
 // headers
@@ -14,8 +14,9 @@
 
 #include <bits/stdc++.h>
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+
 #endif
 
 // clang-format off
@@ -24,17 +25,17 @@ std::mt19937_64 RNG(std::chrono::high_resolution_clock::now().time_since_epoch()
 // modified custom hash to be used with templates from https://codeforces.com/blog/entry/62393
 template<typename T>
 struct custom_hash {
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
 
-  size_t operator()(T x) const {
-    static const uint64_t FIXED_RANDOM = RNG();
-    return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
-  }
+    size_t operator()(T x) const {
+        static const uint64_t FIXED_RANDOM = RNG();
+        return splitmix64(std::hash<T>()(x) + FIXED_RANDOM);
+    }
 };
 
 // aliases
@@ -44,9 +45,6 @@ using pl = std::pair<ll, ll>;
 using vl = std::vector<ll>;
 using vvl = std::vector<vl>;
 using vpl = std::vector<pl>;
-template <typename key, typename value> using pbds_unordered_map = __gnu_pbds::gp_hash_table<key, value, custom_hash<key>>; // 3x - 6x faster than stl unordered_map
-template <typename T> using pbds_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, std::less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>; // find_by_order, order_of_key
-
 // constants
 static constexpr long double EPS(1e-9);
 static constexpr long long MOD(1e9 + 7);  //  or (119 << 23) + 1; primitive_root = 3; // = 998244353
@@ -68,7 +66,7 @@ static constexpr long double PI(3.14159265358979323846);
 #define mk(arr, n, type) type* arr = new type[n]
 #define low(x) char(x | ' ')
 // #define upp(x) char(x & '_')
-#define upp(x) char(x & ~' ')
+#define upp(x) char(x & '_')
 #define sz(x) x.size()
 
 // template functions
@@ -89,39 +87,46 @@ template<typename T_1, typename T_2> std::ostream& operator<<(std::ostream &os, 
 
 // debugging
 #ifdef DYSLEVIUM
-  #define deb(x) std::cerr << #x << " = " << x << '\n'
+    #define deb(x) std::cerr << #x << " = " << x << '\n'
 #else
-  #define deb(x)
+    #define deb(x)
 #endif
 
 inline void solve();
 
 inline void setup(){
-  // std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr)->sync_with_stdio(false);
+    // std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr)->sync_with_stdio(false);
 }
 
 int main(int argc, char* argv[]) {
-  setup();
+    setup();
 
-  auto startTime = std::chrono::high_resolution_clock::now();
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-  ll t = 1;
-  std::cin >> t;
+    ll t = 1;
+    std::cin >> t;
 
-  while (t--) solve();
+    while (t--) solve();
 
-  auto endTime = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
-  #ifdef DYSLEVIUM
-    std::cerr << "\nTime: " << duration.count();
-  #endif
+    #ifdef DYSLEVIUM
+        std::cerr << "\nTime: " << duration.count();
+    #else
+    #ifndef ONLINE_JUDGE  // runs automatically for supported online judges
+        freopen("input.txt", "r", stdin);
+        // freopen("output.txt", "w", stdout);
+    #endif
+    #endif
 
-  return 0;
+    return 0;
 }
 
 using namespace std;
 
 // clang-format on
-inline void solve() {}
+inline void solve() {
+    
+}
